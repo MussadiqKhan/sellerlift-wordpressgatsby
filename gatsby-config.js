ac560d46096f6b57f7b1291acf86e32bbdbd27a5
MussadiqKhan <musadiq.khan@gmail.com>
2020-07-26 12:04:56 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,9 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress-experimental`,
       options: {
-        url:
-          process.env.WPGRAPHQL_URL ||
-          `https://dev-gatsby-source-wordpress-v4.pantheonsite.io/graphql`,
+        url: `http://www.sellerlift.com/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: true,
@@ -30,16 +28,6 @@ module.exports = {
         debug: {
           graphql: {
             writeQueriesToDisk: true,
-          },
-        },
-        type: {
-          Post: {
-            limit:
-              process.env.NODE_ENV === `development`
-                ? // Lets just pull 50 posts in development to make it easy on ourselves.
-                  50
-                : // and we don't actually need more than 5000 in production for this particular site
-                  5000,
           },
         },
       },
